f73224f6d7099abd8726cda0593b63c9e1bf0570
Bruna Zamith <41006576+bzamith@users.noreply.github.com>
2018-12-14 16:40:21 -0200

    Correção de erros

--- classifier.py
+++ classifier.py
@@ -9,32 +9,37 @@ class Classifier:
 		self.count_vect = CountVectorizer()
 		self.tf_transformer = TfidfTransformer()
 		#data read from tsv file
-		self.data_read = self.read_dataset(file_name)
+		self.data_read, self.known_positive = self.read_dataset(file_name)
 		#data_p is the preprocessed data (data we are going to predict)
 		self.data_p = self.data_read[0:]
-		self.known_positive=''
 		if pre_process:
 			self.data_p,self.positive_ex,self.negative_ex = self.process_dataset()
 		#data_t is data_p after being transformeed (count vect and tf_transformer)
-		self.data_t,self.data_train,self.data_test = self.splidata_pset()
+		self.data_t,self.data_train,self.data_test = self.split_dataset()
 		#prediction with KMeans
 		self.clf = self.clf_KMeans()
 		self.prediction = self.clf.predict(self.data_t)
 		self.prediction = self.convert_prediction()
 		self.write_prediction(save_name,pre_process)
-	def read_dataset(self,file_name,test_proportion=0.2):
+	#read dataset
+	def read_dataset(self,file_name):
+		known_positive = ''
 		with open(file_name, 'r') as dataset_file:
 			data = dataset_file.readlines()
 			data = [item.strip().split('\t') for item in data]
 			data_read = [item[1] for item in data[1:len(data)]]
 		for i,offer in enumerate(data_read):
 			data_read[i] = offer.lower()
-		return data_read
-	def splidata_pset(self,test_proportion=0.2):
+			if 'smartphone' in data_read[i] and known_positive== '':
+				known_positive = data_read[i]
+		return data_read, known_positive
+	#split dataset into train and test
+	def split_dataset(self,test_proportion=0.2):
 		data = self.count_vect.fit_transform(self.data_p)
 		data = self.tf_transformer.fit_transform(data)
 		data_train, data_test = train_test_split(data,test_size=test_proportion)
 		return data,data_train,data_test
+	#pre process the data, this means classifying likely instances based on the occurance of some words
 	def process_dataset(self):
 		positive = []
 		positive_ex = []
@@ -44,11 +49,8 @@ class Classifier:
 		for i,offer in enumerate(data):
 			if offer != self.known_positive:
 				if 'smartphone' in offer:
-					if self.known_positive=='':
-						self.known_positive = offer
-					else:
-						positive.append(i)
-						positive_ex.append(offer)
+					positive.append(i)
+					positive_ex.append(offer)
 				elif 'capa' in offer:
 					negative.append(i)
 					negative_ex.append(offer)
@@ -70,6 +72,7 @@ class Classifier:
 		remove = positive + negative
 		data = np.delete(data,remove)
 		return data,positive_ex,negative_ex
+	#KMeans, it can either pick the best k or use k=2
 	def clf_KMeans(self,best_k=False,threshold=100):
 		if best_k:
 			score_anterior = KMeans(n_clusters=2).fit(self.data_train).score(self.data_test)
@@ -87,11 +90,15 @@ class Classifier:
 			nb_k = 2
 		clf = KMeans(n_clusters=nb_k).fit(self.data_t)
 		return clf
+	#convert classes from K Means to "smartphone" or "not smartphone"
 	def convert_prediction(self):
 		try:
-			index = self.data_p.tolist().index(self.known_positive)
+			if type(self.data_p).__module__ == np.__name__:
+				index = self.data_p.tolist().index(self.known_positive)
+			else:
+				index = self.data_p.index(self.known_positive)
 		except ValueError:
-			print("Know positive not found in your dataset")	
+			print("Known positive not found in your dataset")	
 		positive_class = self.prediction[index]
 		new_prediction = []
 		for item in self.prediction:
@@ -100,6 +107,7 @@ class Classifier:
 			else:
 				new_prediction.append("not_smartphone")
 		return new_prediction
+	#extract prediction
 	def write_prediction(self,save_name,pre_process):
 		with open(save_name, 'w') as f:
 			for i,item in enumerate(self.prediction):
